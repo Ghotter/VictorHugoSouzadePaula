81b6c811aa7fa2a6ee22ac1324e231849276922c
victor.hspaula2 <victor.hspaula2@SBC0601498W10-1>
2022-04-26 17:24:17 -0300

    Adicionar arquivos de projeto.

---
+++ VictorHugoSouzadePaula/View/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VictorHugoSouzadePaula.View
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rtb.Text = String.Empty;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "abrira arquivo";
+            openFileDialog1.FileName = "";
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Arquivo txt|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader leitor = new System.IO.StreamReader(openFileDialog1.FileName);
+                rtb.Text = leitor.ReadToEnd();
+                leitor.Close();
+            }
+
+        }
+    }
+}
